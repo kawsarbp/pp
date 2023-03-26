0957539bed8e67dd96ccab9cc8b600eac2de8513
Kawsar Ahmed Ashif <kawsarfaz100@gmail.com>
2023-03-26 14:34:08 +0600

    product show in front end

--- app/Http/Controllers/Frontend/SiteController.php
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,84 +14,99 @@ class SiteController extends Controller
     /*view index page*/
     public function index()
     {
+
         return view('frontend.home-page');
         /*frontend.home-page*/
     }
+
     /*view contact page*/
     public function contact()
     {
         return view('frontend.contact-page');
     }
+
     /*view user home page*/
     public function home()
     {
-        if (Auth::id())
-        {
+        $brands = Brand::where('status','active')->orderBy('id','desc')->limit(12)->get();
+        $products = Product::with('brand', 'subcategory', 'user')->where('status', 'active')->orderBy('id','desc')->get();
+        $productslimit = Product::with('brand', 'subcategory', 'user')->where('status', 'active')->orderBy('id','desc')->limit(6)->get();
+
+        if (Auth::id()) {
             $admin = Auth::user()->role == 1;
-            if ($admin)
-            {
+            if ($admin) {
                 return redirect()->route('dashboard');
-            }else{
-                return view('frontend.ecom.home');
+            } else {
+                return view('frontend.ecom.home', compact('products','productslimit','brands'));
             }
         }
-
-        return view('frontend.ecom.home');
+        return view('frontend.ecom.home', compact('products','productslimit','brands'));
     }
+
     /*view contact us page*/
     public function contactUs()
     {
         return view('frontend.ecom.contact-us');
     }
+
     /*view about us page*/
     public function aboutUs()
     {
         return view('frontend.ecom.about-us');
     }
+
     /*view blog page*/
     public function blog(Request $request)
     {
         $value = $request->search;
-        $search = Blog::where('title','LIKE','%'.$value.'%')->get();
+        $search = Blog::where('title', 'LIKE', '%' . $value . '%')->get();
 
-        $blogs = Blog::where('status','active')->get();
-        return view('frontend.ecom.blog',compact('blogs','search'));
+        $blogs = Blog::where('status', 'active')->get();
+        return view('frontend.ecom.blog', compact('blogs', 'search'));
     }
+
     /*view addToCart page*/
     public function addToCart()
     {
         return view('frontend.ecom.cart.cart');
     }
+
     /*view addToCart page*/
     public function myWishlist()
     {
         return view('frontend.ecom.wishlist.wishlist');
     }
+
     /*view userDashboard  page*/
     public function userDashboard()
     {
         return view('frontend.ecom.user.dashboard');
     }
+
     /*view userDashboard  page*/
     public function myHistories()
     {
         return view('frontend.ecom.user.histories');
     }
+
     /*view checkout  page*/
     public function checkout()
     {
         return view('frontend.ecom.cart.checkout');
     }
+
     /*view shippingMethod  page*/
     public function shippingMethod()
     {
         return view('frontend.ecom.cart.shipping');
     }
+
     /*view shippingMethod  page*/
     public function paymentMethod()
     {
         return view('frontend.ecom.cart.payment_method');
     }
+
     /*view shippingMethod  page*/
     public function orderDetails()
     {
@@ -101,23 +118,24 @@ class SiteController extends Controller
     {
         return view('frontend.ecom.product.product-details');
     }
+
     /*single blog post*/
     public function blogPost($id)
     {
         $blog = Blog::findOrfail($id);
-        if($blog)
-        return view('frontend.ecom.single-blog',compact('blog'));
+        if ($blog)
+            return view('frontend.ecom.single-blog', compact('blog'));
         else
             return redirect()->back();
     }
+
     /*blog post search*/
     public static function blogSearch(Request $request)
     {
         $value = $request->search;
-
-        $search = Blog::where('title','LIKE','%'.$value.'%')->get();
-        $blogs = Blog::where('status','active')->get();
-        return view('frontend.ecom.blog',compact('blogs','search'));
+        $search = Blog::where('title', 'LIKE', '%' . $value . '%')->get();
+        $blogs = Blog::where('status', 'active')->get();
+        return view('frontend.ecom.blog', compact('blogs', 'search'));
     }
 
 }
