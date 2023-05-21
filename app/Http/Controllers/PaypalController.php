eb03a4ee5be07c1c787a0d93821cdd2a52464eaf
Kawsar Ahmed Ashif <kawsarfaz100@gmail.com>
2023-05-21 10:06:58 +0600

    payment gateway with paypal done

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 
@@ -44,16 +49,15 @@ class PaypalController extends Controller
                     return redirect()->away($links['href']);
                 }
             }
-
-
             return redirect()
                 ->route('user.home')
-                ->with('error', 'Something went wrong.');
+                ->with(['type' => 'error', 'message' => 'something wrong!']);
 
         } else {
             return redirect()
                 ->route('user.home')
-                ->with('error', $response['message'] ?? 'Something went wrong.');
+                ->with(['type' => 'error', 'message' => 'something wrong!']);
+
         }
     }
 
@@ -65,13 +69,53 @@ class PaypalController extends Controller
         $response = $provider->capturePaymentOrder($request['token']);
 
         if (isset($response['status']) && $response['status'] == 'COMPLETED') {
-            return redirect()
+
+            $carts = Cart::where('user_id', Auth::id())->get();
+            foreach ($carts as $cart) {
+                Order::create([
+                    'user_id' => Auth::id(),
+                    'product_id' => $cart->product_id,
+                    'cart_id' => $cart->id,
+                    'order_id' => date('dmyhis') . uniqid(),
+                    'name' => $cart->name,
+                    'email' => $cart->email,
+                    'address' => $cart->address,
+                    'phone' => $cart->phone,
+                    'product_name' => $cart->product_name,
+                    'product_price' => $cart->product_price,
+                    'product_discount' => $cart->product_discount,
+                    'product_qty' => $cart->product_qty,
+                    'product_photo' => $cart->product_photo,
+                    'payment_status' => 'Cash On Delivery',
+                    'delivery_status' => 'processing',
+                ]);
+
+                /*send mail*/
+                $data = ['name' => auth()->user()->name, 'greeting' => 'Thank you for your order!', 'status' => 'processing',];
+                $user['to'] = auth()->user()->email;
+                Mail::send('mail/order-mail', $data, function ($message) use ($user) {
+                    $message->to($user['to']);
+                    $message->subject('Faz Group');
+                });
+                /*send mail*/
+                /*update product qty*/
+                $product = Product::find($cart->product_id);
+                $product->product_quantity = $product->product_quantity - $cart->product_qty;
+                $product->save();
+                /*update product qty*/
+            }
+            /*delete product*/
+            $ids = Cart::where('user_id', Auth::id())->pluck('id')->toArray();
+            Cart::whereIn('id', $ids)->delete();
+            return redirect()->route('user.paymentMethod')->with(['type' => 'success', 'message' => 'Thank you for your purchase!']);
+            /*delete product*/
+            /*return redirect()
                 ->route('user.home')
-                ->with('success', 'Transaction complete.');
+                ->with('success', 'Transaction complete.');*/
         } else {
             return redirect()
                 ->route('user.home')
-                ->with('error', $response['message'] ?? 'Something went wrong.');
+                ->with(['type' => 'error', 'message' => 'something wrong!']);
         }
     }
 
@@ -80,7 +124,7 @@ class PaypalController extends Controller
     {
         return redirect()
             ->route('user.home')
-            ->with('error', $response['message'] ?? 'You have canceled the transaction.');
+            ->with(['type' => 'error', 'message' => 'something wrong!']);
     }
 
 
