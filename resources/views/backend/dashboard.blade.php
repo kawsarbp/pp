3d4168c0a3ff1112cdcceca8980f05d6d891c349
Kawsar Ahmed Ashif <kawsarfaz100@gmail.com>
2023-05-22 11:28:34 +0600

    bug fix

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -47,8 +47,8 @@
                     <div class="d-flex flex-row">
                         <div class="round round-lg align-self-center round-info"><i class="ti-wallet"></i></div>
                         <div class="m-l-10 align-self-center">
-                            <h3 class="m-b-0 font-light">{{ $toalRevenue }}</h3>
-                            <h5 class="text-muted m-b-0">$ Total Revenue</h5></div>
+                            <h3 class="m-b-0 font-light">$ {{ $toalRevenue }}</h3>
+                            <h5 class="text-muted m-b-0"> Total Revenue</h5></div>
                     </div>
                 </div>
             </div>
